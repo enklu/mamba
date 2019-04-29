823e833fde6f08573673be2f1e06f18e2605f6c4
James Sundra <pojo226@gmail.com>
2019-04-29 12:41:54 -0700

    Move helpers to Util. Add logging for body deletion in all cases

--- Mamba/Kinect/Util.cs
+++ Mamba/Kinect/Util.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using Enklu.Data;
 using Enklu.Mamba.Network;
+using Microsoft.Kinect;
 
 namespace Enklu.Mamba.Kinect
 {
@@ -40,6 +42,61 @@ namespace Enklu.Mamba.Kinect
             elm.Schema.Strings["assetSrc"] = asset;
             elm.Type = ElementTypes.Asset;
             return elm;
+        }
+        
+        /// <summary>
+        /// Finds an element with a matching KinectId.
+        /// </summary>
+        /// <param name="kinectId"></param>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        public static ElementData FindKinect(string kinectId, ElementData element)
+        {
+            element.Schema.Strings.TryGetValue("kinect.id", out var schemaId);
+
+            if (schemaId == kinectId)
+            {
+                return element;
+            }
+            
+            for (int i = 0, len = element.Children.Length; i < len; i++)
+            {
+                var search = FindKinect(kinectId, element.Children[i]);
+                if (search != null)
+                {
+                    return search;
+                }
+            }
+
+            return null;
+        }
+        
+        /// <summary>
+        /// Determines which joints to track based on schema, and their corresponding assets
+        /// </summary>
+        /// <param name="elementData"></param>
+        /// <returns></returns>
+        public static Dictionary<JointType, string> BuildTracking(ElementData elementData)
+        {
+            const string kinectBodyPrefix = "kinect.body.";
+            var prefixLen = kinectBodyPrefix.Length;
+
+            var jointMap = new Dictionary<JointType, string>();
+            foreach (var kvp in elementData.Schema.Strings)
+            {
+                if (kvp.Key.StartsWith(kinectBodyPrefix))
+                {
+                    var bodyPart = kvp.Key.Substring(prefixLen);
+                    bodyPart = bodyPart.Substring(0, bodyPart.IndexOf(".", StringComparison.Ordinal));
+
+                    if (Enum.TryParse<JointType>(bodyPart, true, out var jointType))
+                    {
+                        jointMap[jointType] = kvp.Value;
+                    }
+                }
+            }
+
+            return jointMap;
         }
     }
 }
